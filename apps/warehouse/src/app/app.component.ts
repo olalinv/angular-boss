e7f430150b4be9e49fc97b96649697be351253bd
Óscar <oscar.lalin@coremain.com>
2020-04-11 00:12:57 +0200

    9. Web Components

--- apps/warehouse/src/app/app.component.ts
+++ apps/warehouse/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'ab-warehouse-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'warehouse';
+  public building = {
+    date: Date.now(),
+    value: 2345.897,
+    status: 'buy'
+  };
+  constructor() {
+    if (this.building.status === 'buy') {
+      this.building.status = environment.buy;
+    } else {
+      this.building.status = environment.sell;
+    }
+  }
 }
